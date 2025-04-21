a1451ce3c8fe12fc4c34958fde4094c4eb94e760
Naveendinula <59760329+Naveendinula@users.noreply.github.com>
2025-04-20 21:59:51 -0500

    site update
    feat: add Home page component and update navigation sidebar

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,95 +1,35 @@
 import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import Typewriter from "typewriter-effect";
-import { introdata, meta } from "../../content_option";
-import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import { meta } from "../../content_option";
 
-export const Home = () => {
+const Home = () => {
   return (
     <HelmetProvider>
-      <section id="home" className="home">
+      <Container className="home-container">
         <Helmet>
           <meta charSet="utf-8" />
-          <title> {meta.title}</title>
+          <title>Home | {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
-        <div className="gradient-bg">
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <filter id="goo">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                <feColorMatrix
-                  in="blur"
-                  mode="matrix"
-                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                  result="goo"
-                />
-                <feBlend in="SourceGraphic" in2="goo" />
-              </filter>
-            </defs>
-          </svg>
-          <div className="gradients-container">
-            <div className="g1"></div>
-            <div className="g2"></div>
-            <div className="g3"></div>
-            <div className="g4"></div>
-            <div className="g5"></div>
-            <div className="interactive"></div>
-          </div>
-          <div className="gradients-container">
-            <div className="g1"></div>
-            <div className="g2"></div>
-            <div className="g3"></div>
-            <div className="g4"></div>
-            <div className="g5"></div>
-            <div className="interactive"></div>
-          </div>
-        </div>
-        
-        <div className="intro_sec d-flex align-items-center justify-content-center">
-          <div className="text h-100 d-flex justify-content-center">
-            <div className="align-self-center">
-              <div className="intro mx-auto">
-                <h2 className="mb-1x">{introdata.title}</h2>
-                <h1 className="fluidz-48 mb-1x">
-                  <Typewriter
-                    options={{
-                      strings: [
-                        introdata.animated.first,
-                        introdata.animated.second,
-                        introdata.animated.third,
-                      ],
-                      autoStart: true,
-                      loop: true,
-                      deleteSpeed: 10,
-                    }}
-                  />
-                </h1>
-                <p className="mb-1x">{introdata.description}</p>
-                
-                <div className="availability-indicator mb-4">
-                  <div className="availability-dot"></div>
-                  <span>Available for work</span>
-                </div>
-                
-                <div className="intro_btn-action pb-5">
-                  <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn">
-                      My Portfolio
-                    </div>
-                  </Link>
-                  <Link to="/contact">
-                    <div id="button_h" className="ac_btn">
-                      Contact Me
-                    </div>
-                  </Link>
-                </div>
-              </div>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="12">
+            <h1 className="display-4 mb-4">Home</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="12">
+            {/* This container is intentionally left blank as requested */}
+            <div className="home-content-placeholder">
+              {/* Blank content area */}
             </div>
-          </div>
-        </div>
-      </section>
+          </Col>
+        </Row>
+      </Container>
     </HelmetProvider>
   );
 };
+
+export default Home;
